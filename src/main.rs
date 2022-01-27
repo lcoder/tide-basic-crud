5fdb9ed4b985d3bfe46c8ae5807124bd62833222
毛庭峰😈 <maotingfeng@youzan.com>
2022-01-27 14:12:05 +0800

    11 std::io::Write vec.write_all flush

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,16 @@
-
-#[derive(Debug)]
-struct TreeNode<T> {
-  pub element: T,
-  left: BinaryTree<T>,
-  right: BinaryTree<T>,
-}
-
-#[derive(Debug)]
-enum BinaryTree<T> {
-  Empty,
-  NonEmpty(Box<TreeNode<T>>),
-}
-
-impl<T: Copy + Ord> BinaryTree<T> {
-  fn add(&mut self, value: T) {
-      match *self {
-        BinaryTree::Empty => {
-          let new = TreeNode { element: value, left: BinaryTree::Empty, right: BinaryTree::Empty };
-          let new = Box::new(new);
-          *self = BinaryTree::NonEmpty(new);
-        }
-
-        BinaryTree::NonEmpty(ref mut node) => {
-          let has = value <= node.element;
-          if has {
-            node.left.add(value);
-          } {
-            node.right.add(value);
-          }
-        }
-      }
-  }
-}
-
+use std::io::Write;
 
 #[async_std::main]
 async fn main() {
-  let mut tree = BinaryTree::Empty;
-  tree.add("hi");
-  tree.add("maotingfeng");
+  let mut bytes: Vec<u8> = vec![];
+
+  say_hello(&mut bytes);
+
+  println!("list={:?}", bytes);
+}
 
 
-  if let BinaryTree::NonEmpty(aa) = tree {
-    println!("tree={:?}", aa.element);
-  }
-
+fn say_hello(out: &mut dyn Write) {
+  out.write_all(b"hello world\n");
+  out.flush();
 }
