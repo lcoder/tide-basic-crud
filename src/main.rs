20245981071dfa36dfa9c93530a8fa3589c5e2b2
毛庭峰😈 <maotingfeng@youzan.com>
2022-01-27 13:31:48 +0800

    10.2.8 填充二叉树(修改点,增加了Copy trait的限制)

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 
-#[async_std::main]
-async fn main() {
-  let x = 10;
-  
-  let rx = &x;
-
-  assert_eq!(rx + 1, 11);
-
-  let account = Account { name: "maotingfeng".to_string(), language: "chinese".to_string() };
-
-  let a = match account {
-    Account { ref name, .. } => {
-      greet(&name);
-      2
-    },
-    _ => 1,
-  };
-  let bbb = account;
-}
-
-fn greet(name: &str) -> i32 {
-  println!("{:?}", name);
-  return 2;
+#[derive(Debug)]
+struct TreeNode<T> {
+  pub element: T,
+  left: BinaryTree<T>,
+  right: BinaryTree<T>,
 }
 
 #[derive(Debug)]
-struct Account {
-  name: String,
-  language: String,
+enum BinaryTree<T> {
+  Empty,
+  NonEmpty(Box<TreeNode<T>>),
 }
 
+impl<T: Copy + Ord> BinaryTree<T> {
+  fn add(&mut self, value: T) {
+      match *self {
+        BinaryTree::Empty => {
+          let new = TreeNode { element: value, left: BinaryTree::Empty, right: BinaryTree::Empty };
+          let new = Box::new(new);
+          *self = BinaryTree::NonEmpty(new);
+        }
+
+        BinaryTree::NonEmpty(ref mut node) => {
+          let has = value <= node.element;
+          if has {
+            node.left.add(value);
+          } {
+            node.right.add(value);
+          }
+        }
+      }
+  }
+}
+
+
+#[async_std::main]
+async fn main() {
+  let mut tree = BinaryTree::Empty;
+  tree.add("hi");
+  tree.add("maotingfeng");
+
+
+  if let BinaryTree::NonEmpty(aa) = tree {
+    println!("tree={:?}", aa.element);
+  }
+
+}
