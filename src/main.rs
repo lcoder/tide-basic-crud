851153eb40a1a0f0ad7dcc2ef5678c622f7100b3
毛庭峰😈 <maotingfeng@youzan.com>
2022-03-19 15:28:08 +0800

    feat: 一个模仿python的httpie程序

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,158 @@
-#[derive(Debug)]
-enum Gender {
-    Unspecified = 0,
-    Female = 1,
-    Male = 2,
+use clap::{Parser};
+use mime::Mime;
+use anyhow::{anyhow, Result};
+use colored::Colorize;
+use std::{str::FromStr, collections::HashMap};
+use reqwest::{header, Client, Response, Url};
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
 }
 
-#[derive(Debug, Copy, Clone)]
-struct  UserId(u64);
+#[derive(Parser, Debug)]
+#[clap(version = "1.0.-beta", author = "happy写的命令行工具 <tyr@chen.com>")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
 
-#[derive(Debug, Copy, Clone)]
-struct TopicId(u64);
+#[derive(Parser, Debug)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
 
-#[derive(Debug)]
-struct User {
-    id: UserId,
-    name: String,
-    gender: Gender,
+#[derive(Parser, Debug)]
+struct Get {
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+}
+
+#[derive(Parser, Debug)]
+struct Post {
+    /// HTTP 请求的 URL
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
 #[derive(Debug)]
-struct Topic {
-    id: TopicId,
-    name: String,
-    owner: UserId,
+struct KvPair {
+    k: String,
+    v: String,
 }
 
-#[derive(Debug)]
-enum Event {
-    Join((UserId, TopicId)),
-    Leave((UserId, TopicId)),
-    Message((UserId, TopicId, String)),
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split('=');
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
 }
 
-fn main() {
-    let alice = User { id: UserId(1), name: "Alice".into(), gender: Gender::Female };
-    let bob = User { id: UserId(2), name: "Bolb".into(), gender: Gender::Male };
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
 
-    let topic = Topic {
-        id: TopicId(1),
-        name: "rust".into(),
-        owner: UserId(1),
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), &resp.status()).blue();
+    println!("{}\n", status);
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    println!("\n");
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(
+            |v| v
+                .to_str()
+                .unwrap()
+                .parse()
+                .unwrap()
+        )
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        _ => println!("{}", body),
+    }
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main () -> Result<()> {
+    let opts: Opts = Opts::parse();
+
+    println!("参数={:?}", opts);
+
+    let mut headers = header::HeaderMap::new();
+    
+    headers.insert(
+        "x-powered-by",
+        "Rust"
+        .parse()
+        ?
+    );
+    headers.insert(header::USER_AGENT, "rust httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
     };
+    Ok(result)
+}
 
-    let event1 = Event::Join((alice.id, topic.id));
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let event2 = Event::Leave((bob.id, topic.id));
-
-    let event3 = Event::Message((alice.id, topic.id, "Hello world!".into()));
-
-    println!("event1: {:?}, event2: {:?}, event3: {:?}", event1, event2, event3);
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
 }
